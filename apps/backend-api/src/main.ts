2af5434585d1fbd034bb98f74096903744ab23eb
Rodan Ramdam <rodan.ramdam@gmail.com>
2022-07-07 16:16:45 +0545

    feat: setup for validation

--- apps/backend-api/src/main.ts
+++ apps/backend-api/src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = app.get(Logger);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
   const port = 3001;
   await app.listen(port, () => {
     logger.log(`starting server in :${port}`);
