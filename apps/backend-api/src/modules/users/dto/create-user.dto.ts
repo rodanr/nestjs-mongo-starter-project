8681cbcdd6af21a0520da26ee07c64b9129cb00d
Rodan Ramdam <rodan.ramdam@gmail.com>
2022-07-07 17:56:32 +0545

    feat: add validation to dtos using class validator

--- apps/backend-api/src/modules/users/dto/create-user.dto.ts
+++ apps/backend-api/src/modules/users/dto/create-user.dto.ts
@@ -1,13 +1,29 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @MinLength(3)
   readonly username: string;
-
+  @IsString()
+  @IsNotEmpty()
   readonly firstName: string;
-
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
   readonly middleName?: string;
-
+  @IsString()
+  @IsNotEmpty()
   readonly lastName: string;
-
+  @IsEmail()
+  @IsNotEmpty()
   readonly email: string;
-
+  @IsString()
+  @MinLength(8)
   readonly password: string;
 }
