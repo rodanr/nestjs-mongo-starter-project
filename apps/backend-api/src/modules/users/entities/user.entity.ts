61f5fa8c82432a1e0c398bef10f2d6f169b77872
Rodan Ramdam <rodan.ramdam@gmail.com>
2022-07-07 15:58:29 +0545

    fix: make password field as non selectable property and add id as new uuid field

--- apps/backend-api/src/modules/users/entities/user.entity.ts
+++ apps/backend-api/src/modules/users/entities/user.entity.ts
@@ -3,6 +3,8 @@ import { Document } from 'mongoose';
 
 @Schema()
 export class User extends Document {
+  @Prop({ unique: true })
+  id: string;
   @Prop({ unique: true, minlength: 3 })
   username: string;
   @Prop()
@@ -13,7 +15,7 @@ export class User extends Document {
   lastName: string;
   @Prop({ unique: true })
   email: string;
-  @Prop({ minlength: 8 })
+  @Prop({ minlength: 8, select: false })
   password: string;
   @Prop({ type: Date })
   created_at: Date;
